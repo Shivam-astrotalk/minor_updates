fe6d684d99068420e859c9ee8bbc2da7219a38bb
ashish <aj@astrotalk.com>
2020-12-30 19:18:10 +0530

    product list

--- src/main/java/com/astrotalk/live/network/WalletServiceClient.java
+++ src/main/java/com/astrotalk/live/network/WalletServiceClient.java
@@ -3,11 +3,12 @@ package com.astrotalk.live.network;
 import org.json.JSONObject;
 import retrofit2.Call;
 import retrofit2.http.GET;
+import retrofit2.http.POST;
 import retrofit2.http.Query;
 
 public interface WalletServiceClient {
 
-    @GET("/AstroTalk/wallet/deduction")
+    @POST("/AstroTalk/wallet/deduction")
     public Call<JSONObject> deductUserWallet(@Query("amount") double amount, @Query("comment") String comment, @Query("orderId") long eventId,
                                              @Query("secretKey") String key, @Query("serviceId") String serviceId, @Query("userId") long userId);
 
