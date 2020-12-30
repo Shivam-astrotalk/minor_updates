0f46b35a4d46b02105b680ed5d27c3d33a42e33b
ashish <aj@astrotalk.com>
2020-12-30 21:26:26 +0530

    product list

--- src/main/java/com/astrotalk/live/AstroYetiApplication.java
+++ src/main/java/com/astrotalk/live/AstroYetiApplication.java
@@ -1,6 +1,7 @@
 package com.astrotalk.live;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
@@ -12,7 +13,7 @@ import java.io.IOException;
 
 @EnableAsync
 @EnableScheduling
-@EnableTransactionManagement
+@EnableAutoConfiguration
 @SpringBootApplication(exclude = {
 		org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration.class, org.springframework.boot.autoconfigure.gson.GsonAutoConfiguration.class}
 )
