80be26132323613587df37143725fe645ec4c0c0
ashish <aj@astrotalk.com>
2020-12-31 14:35:18 +0530

    changes

--- src/main/java/com/astrotalk/live/service/LiveService.java
+++ src/main/java/com/astrotalk/live/service/LiveService.java
@@ -241,7 +241,7 @@ public class LiveService {
 
     public void checkAndDeductMoney(long userId, double money, String message, long eventId, long purchaseId) throws LiveException {
         //throw new LiveException("Not enough money, please recharge");
-        Call<com.astrotalk.live.model.pojo.Response> call = walletServiceClient.deductUserWallet(-1.0*money, URLEncoder.encode(message),purchaseId,walletSecretKey,"6",userId);
+        Call<com.astrotalk.live.model.pojo.Response> call = walletServiceClient.deductUserWallet(money, URLEncoder.encode(message),purchaseId,walletSecretKey,"6",userId);
         try{
             Response<com.astrotalk.live.model.pojo.Response> response = call.execute();
             log.info("Response : {}", response);
